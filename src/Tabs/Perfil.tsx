c4e4b41cc08607d68d02253af44dda62d990b719
Eduarda <143120573+duardasc@users.noreply.github.com>
2024-05-29 14:30:25 -0300

    ATIVIDADE LOGIN
    Deslogar e voltar para login

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -4,8 +4,9 @@ import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { pegarDadosPaciente } from '../servicos/PacienteServico'
 import { Paciente } from '../interfaces/Paciente'
+import { Botao } from '../componentes/Botao'
 
-export default function Perfil(){
+export default function Perfil({ navigation } : any){
 
   const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
 
@@ -22,6 +23,11 @@ export default function Perfil(){
     }
     dadosPaciente()
   })
+
+  function deslogar(){
+    AsyncStorage.removeItem('token')
+    navigation.replace('Login')
+  }
   
   return(
     <ScrollView flex={1}>
@@ -37,9 +43,13 @@ export default function Perfil(){
 
         <Divider mt={5} />
 
-        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
-        <Text>Bronquite</Text>
-        <Text>Sinusite</Text>
+        <Titulo color="blue.500" mb={1}>Planos de saúde</Titulo>
+        {
+          dadosPaciente.planosSaude?.map((plano, index) => (
+            <Text key = {index}>{plano}</Text>
+          ))
+        }
+        <Botao onPress={deslogar}>Deslogar</Botao>
       </VStack>
     </ScrollView>
   )
